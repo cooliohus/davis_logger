401766bbb3f7d81e7c41f9aeb1cee314ad10ec82
Andy Cooper <andy@coolioh.com>
2024-10-20 12:17:47 -0400

    First Release (seconf time :-) )

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,12 @@
 * logger challenge.  The program size is such that it should work     *
 * with ATTINY85 and ATTINY45 (not tested) devices.                    *
 *                                                                     *
-* The code is based on work and code done by Torkel M. Jodalen and    *
-* others which is documkented here:                                   *
+* The code is based on work and code created by Torkel M. Jodalen     *
+* and others which is documented here:                                *
 *       https://www.annoyingdesigns.com/meteo/DavisSPI.pdf            *
 *                                                                     *
 * Additional information and the original ATTINY85 hex file can be    *
-* found here.  USe the .txt file as the .hex file appears empty.      *
+* found here.                                                         *
 * https://github.com/SpiceWire/davis-vantage-pro2-datalogger-emulator *
 *                                                                     *
 *                                                                     *
